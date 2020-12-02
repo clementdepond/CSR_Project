c010c747b28bf624beee0ada88c853efe763f573
clementdepond <70962478+clementdepond@users.noreply.github.com>
2020-12-02 11:53:57 +0100

    Add files via upload

---
+++ src/Requin.java
@@ -0,0 +1,15 @@
+public class Requin {
+    private int NB_CYCLE;
+    private int NB_PILOTES;
+
+    public Requin() {
+        this.NB_CYCLE = 10;
+        this.NB_PILOTES = 5;
+    }
+
+    private void seDeplacer() {}
+
+    public void run() {
+        
+    }
+}
